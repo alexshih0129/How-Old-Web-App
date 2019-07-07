35ccc29cd62cede813c4c4d7fa54466ad3bd5a5f
alexshih0129 <alexshih0129@hotmail.com>
2019-07-07 23:22:50 +1200

    Move
    Move project file

---
+++ my-app/src/components/Result/Result.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+
+
+interface IProps{
+  result:string
+  filelength:any
+}
+
+export default class Result extends React.Component<IProps,{}> {
+  
+  public render() {
+    return (
+      <div className="dank">
+        {
+         
+        }
+      </div>
+    )
+  }
+}
